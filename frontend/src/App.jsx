8a8e4be8d4594c8f4d6af50dc2689efa88cc674d
unknown <tayanesilvasouzas@gmail.com>
2020-12-10 18:35:23 -0300

    primeira parte do front

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from  'react';
+
+import Home from './Pages/Home';
+
+import './assets/styles/global.css';
+
+const App = () => {
+    return(
+   <>
+     
+<Home/>
+   </>
+
+    )
+}
+
+export default App; 
---
+++ frontend/src/Pages/Home/index.jsx
@@ -0,0 +1,43 @@
+import React from  'react';
+
+import './styles.css';
+
+import yoda from '../../assets/images/yoda.png';
+import separator from '../../assets/images/separator.svg';
+
+const Home = () => {
+    return(
+   <>
+     <div className="section-1">
+      <div className="container">
+         <header className="header">
+             <h2>Luke Code</h2>
+             <nav>
+                 <button>Fazer login</button>
+                 <button>Escreva-se</button>
+             </nav>
+         </header>
+         <main>
+             <section>
+                 <h1>Se torce um mestre<br/> da programação!</h1>
+                 <img src={separator} alt="icone" />
+                 <p>
+                     De o próximo passo para seu futuro,<br/>
+                     que a força esteja com você.
+                 </p>
+             </section>
+
+             <section>
+             <img src={yoda} alt="yoda - star wars" />
+             </section>
+       </main>
+      </div>
+
+     </div>
+
+   </>
+
+    )
+}
+
+export default Home; 
